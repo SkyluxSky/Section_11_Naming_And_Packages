cf08d96ce794ef553ac0edf6a7028d64d1717c65
SkyluxSky <skyluxsky@gmail.com>
2021-07-14 19:09:32 -0400

    JavaFX works!!!

--- Packages/src/com/skyluxsky/Main.java
+++ Packages/src/com/skyluxsky/Main.java
@@ -1,8 +1,14 @@
 package com.skyluxsky;
 
+
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        //Java recommends names of packages that you may want
+        //A package is a method of grouping java classes and interfaces together...
+
+        org.w3c.dom.Node node = null;
     }
 }
