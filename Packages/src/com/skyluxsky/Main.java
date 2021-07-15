3c19b8d06d465a8cac2610c6096d36eeea734e51
SkyluxSky <skyluxsky@gmail.com>
2021-07-14 20:41:34 -0400

    Packages Part 0.5

--- Packages/src/com/skyluxsky/Main.java
+++ Packages/src/com/skyluxsky/Main.java
@@ -1,6 +1,7 @@
 package com.skyluxsky;
 
-
+//import javafx.scene.Node;
+//import org.w3c.dom.Node; //Error we cannot have two imports of the same name.
 
 public class Main {
 
@@ -9,6 +10,24 @@ public class Main {
         //Java recommends names of packages that you may want
         //A package is a method of grouping java classes and interfaces together...
 
-        org.w3c.dom.Node node = null;
+        //javafx.scene.Node node = null;
+
+        javafx.scene.Node node = null; //Referenced from import...
+        org.w3c.dom.Node nodeDifferentPackage = null;
+
+
+            //Some reasons to use packages are:
+            //-Programmers can easily determine that the classes are related.
+
+            //-It is easy to know where to find the classes and interfaces that can provide the functions
+            // provided by the package.
+
+            //-Because the package creates a new namespace, class and interface name conflicts are avoided.
+
+            //Classes within the package can have unrestricted access to one another while
+            //still restricting access for classes outside the package.
+
+            //java.lang is created automatically when we create a java program.
+
     }
 }
